85bfb2bf27ef4cf58ae24a1f5753d408cd070d4b
Adriano Flach <adriano.spintec@oncologiador.corp>
2022-04-20 12:53:05 -0300

    refactor league

--- src/data/_base/BaseLeague.js
+++ src/data/_base/BaseLeague.js
@@ -10,58 +10,30 @@ export default class BaseLeague {
     this.nextRoundName = 'Not implemented';
   }
 
-  static validateClubs(clubs) {
-    if (!Array.isArray(clubs)) {
-      console.error('clubs should be an array of club');
-    } else {
-      for (const club of clubs) {
-        if (!club.id || !club.name) {
-          console.error('clubs should have club with id and name attributes');
-        }
-      }
-    }
-    return clubs;
-  }
-
-  async loadClubs() {
-    console.error('loadClubs not implemented.');
-    return new Promise(resolve => resolve([]));
-  }
-
-  currentRoundName() {
-    console.error('currentRoundName not implemented.');
-    return 'Not implemented';
-  }
-
   currentRoundFixtures() {
     console.error('currentRoundFixtures not implemented.');
     return [];
   }
 
-  teamStanding() {
-    console.error('teamStanding not implemented.');
-    return {
-      rank: 0,
-      points: 0,
-      all: { played: 0, win: 0, draw: 0, lose: 0, goals: { for: 0, against: 0 } },
-      home: { played: 0, win: 0, draw: 0, lose: 0, goals: { for: 0, against: 0 } },
-      away: { played: 0, win: 0, draw: 0, lose: 0, goals: { for: 0, against: 0 } }
-    };
+  lastMatches(teamId, quantity = 6) {
+    return this.fixtures
+      .filter(m => m.homeId == teamId || m.awayId == teamId)
+      .splice(0, quantity)
+      .map(m => m.validateResult(teamId));
   }
 
-  lastHomeMatches() {
-    console.error('lastHomeMatches not implemented.');
-    return [];
+  lastHomeMatches(teamId, quantity = 6) {
+    return this.fixtures
+      .filter(m => m.homeId == teamId)
+      .splice(0, quantity)
+      .map(m => m.validateResult(teamId));
   }
 
-  lastAwayMatches() {
-    console.error('lastAwayMatches not implemented.');
-    return [];
-  }
-
-  lastMatches() {
-    console.error('lastMatches not implemented.');
-    return [];
+  lastAwayMatches(teamId, quantity = 6) {
+    return this.fixtures
+      .filter(m => m.awayId == teamId)
+      .splice(0, quantity)
+      .map(m => m.validateResult(teamId));
   }
 
   loadPreviousFixture() {
@@ -72,5 +44,54 @@ export default class BaseLeague {
   loadNextFixture() {
     this.currentRound += 1;
     this.currentRoundTitle = `Round ${this.currentRound}`;
+  }
+
+  teamStanding(teamId) {
+    const stand = this.standing.find(stand => stand.team.id == teamId);
+
+    const homeStanding = () => {
+      const homeMatches = this.lastHomeMatches(teamId);
+      return {
+        played: homeMatches.length,
+        win: homeMatches.filter(m => m.win).length,
+        draw: homeMatches.filter(m => m.draw).length,
+        lose: homeMatches.filter(m => m.lose).length,
+        goals: {
+          for: homeMatches.map(m => m.homeScore || 0).reduce((a, b) => a + b, 0),
+          against: homeMatches.map(m => m.awayScore || 0).reduce((a, b) => a + b, 0),
+        },
+      };
+    };
+
+    const awayStanding = () => {
+      const awayMatches = this.lastAwayMatches(teamId);
+      return {
+        played: awayMatches.length,
+        win: awayMatches.filter(m => m.win).length,
+        draw: awayMatches.filter(m => m.draw).length,
+        lose: awayMatches.filter(m => m.lose).length,
+        goals: {
+          for: awayMatches.map(m => m.awayScore || 0).reduce((a, b) => a + b, 0),
+          against: awayMatches.map(m => m.homeScore || 0).reduce((a, b) => a + b, 0),
+        },
+      };
+    };
+
+    return {
+      rank: stand.position,
+      points: stand.points,
+      all: {
+        played: stand.playedGames,
+        win: stand.won,
+        draw: stand.draw,
+        lose: stand.lost,
+        goals: {
+          for: stand.goalsFor,
+          against: stand.goalsAgainst
+        }
+      },
+      home: homeStanding(),
+      away: awayStanding(),
+    };
   }
 }
--- src/data/grid-football-data/League.js
+++ src/data/grid-football-data/League.js
@@ -26,34 +26,6 @@ export default class League extends BaseLeague {
     this.currentRoundTitle = league.currentRoundTitle;
   }
 
-  currentRoundName() {
-    return `Round ${this.currentRound}`;
-  }
-
-  async loadRound(roundId) {
-    const getTeamLogo = (teamId) => {
-      const stand = this.standing.find(stand => stand.team.id == teamId);
-      return stand.team.crestUrl;
-    };
-
-    const setMatchLogo = (match) => {
-      return {
-        ...match,
-        homeTeam: {
-          ...match.homeTeam,
-          logo: getTeamLogo(match.homeTeam.id),
-        },
-        awayTeam: {
-          ...match.awayTeam,
-          logo: getTeamLogo(match.awayTeam.id),
-        }
-      };
-    };
-
-    const nextRoundURL = `${URL}/competitions/${this.id}/matches?matchday=${roundId}`;
-    return Cache.get(nextRoundURL, HEADERS).then(data => data.matches.map(m => setMatchLogo(m)));
-  };
-
   async currentRoundFixtures() {
     const loadStanding = () => {
       const standingURL = `${URL}/competitions/${this.id}/standings`;
@@ -65,78 +37,32 @@ export default class League extends BaseLeague {
       return Cache.get(nextRoundURL, HEADERS).then(data => this.fixtures = data.matches.map(m => new Match(m)));
     };
 
+    const loadRound = (roundId) => {
+      const getTeamLogo = (teamId) => {
+        const stand = this.standing.find(stand => stand.team.id == teamId);
+        return stand.team.crestUrl;
+      };
+
+      const setMatchLogo = (match) => {
+        return {
+          ...match,
+          homeTeam: {
+            ...match.homeTeam,
+            logo: getTeamLogo(match.homeTeam.id),
+          },
+          awayTeam: {
+            ...match.awayTeam,
+            logo: getTeamLogo(match.awayTeam.id),
+          }
+        };
+      };
+
+      const nextRoundURL = `${URL}/competitions/${this.id}/matches?matchday=${roundId}`;
+      return Cache.get(nextRoundURL, HEADERS).then(data => data.matches.map(m => setMatchLogo(m)));
+    };
+
     await loadMatches();
     await loadStanding();
-    return this.loadRound(this.currentRound).then(matches => matches.map(match => new Match(match)));
-  }
-
-  teamStanding(teamId) {
-    const stand = this.standing.find(stand => stand.team.id == teamId);
-
-    const homeStanding = () => {
-      const homeMatches = this.lastHomeMatches(teamId);
-      return {
-        played: homeMatches.length,
-        win: homeMatches.filter(m => m.win).length,
-        draw: homeMatches.filter(m => m.draw).length,
-        lose: homeMatches.filter(m => m.lose).length,
-        goals: {
-          for: homeMatches.map(m => m.homeScore || 0).reduce((a, b) => a + b, 0),
-          against: homeMatches.map(m => m.awayScore || 0).reduce((a, b) => a + b, 0),
-        },
-      };
-    };
-
-    const awayStanding = () => {
-      const awayMatches = this.lastAwayMatches(teamId);
-      return {
-        played: awayMatches.length,
-        win: awayMatches.filter(m => m.win).length,
-        draw: awayMatches.filter(m => m.draw).length,
-        lose: awayMatches.filter(m => m.lose).length,
-        goals: {
-          for: awayMatches.map(m => m.awayScore || 0).reduce((a, b) => a + b, 0),
-          against: awayMatches.map(m => m.homeScore || 0).reduce((a, b) => a + b, 0),
-        },
-      };
-    };
-
-    return {
-      rank: stand.position,
-      points: stand.points,
-      all: {
-        played: stand.playedGames,
-        win: stand.won,
-        draw: stand.draw,
-        lose: stand.lost,
-        goals: {
-          for: stand.goalsFor,
-          against: stand.goalsAgainst
-        }
-      },
-      home: homeStanding(),
-      away: awayStanding(),
-    };
-  }
-
-  lastMatches(teamId, quantity = 6) {
-    return this.fixtures
-      .filter(m => m.homeId == teamId || m.awayId == teamId)
-      .splice(0, quantity)
-      .map(m => m.validateResult(teamId));
-  }
-
-  lastHomeMatches(teamId, quantity = 6) {
-    return this.fixtures
-      .filter(m => m.homeId == teamId)
-      .splice(0, quantity)
-      .map(m => m.validateResult(teamId));
-  }
-
-  lastAwayMatches(teamId, quantity = 6) {
-    return this.fixtures
-      .filter(m => m.awayId == teamId)
-      .splice(0, quantity)
-      .map(m => m.validateResult(teamId));
+    return loadRound(this.currentRound).then(matches => matches.map(match => new Match(match)));
   }
 }
