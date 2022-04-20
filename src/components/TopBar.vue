5ab2e427d82817edbb9e9fef036d14abbaf4365b
Adriano Flach <adriano.spintec@oncologiador.corp>
2022-04-20 13:15:38 -0300

    remove league selector

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,11 +4,6 @@
       <select class="custom-select" :value="api" @input="changeApi">
         <option v-for="item in apis" :key="item" :value="item">{{ item }}</option>
       </select>
-      <select class="custom-select ml-1" :value="leagueId" @input="changeLeague">
-        <option value="0">Select the league</option>
-        <option v-for="item in leagues" :key="item.key" :value="item.key">{{ item.value }}</option>
-      </select>
-      <span class="ml-2" v-if="loadingClubs">Loading clubs</span>
     </div>
   </div>
 </template>
@@ -29,7 +24,6 @@ export default {
   methods: {
     ...mapMutations([
       'changeApi',
-      'changeLeague',
     ])
   }
 };
--- src/store.js
+++ src/store.js
@@ -4,9 +4,7 @@ import {
   apis,
   loadLeagues,
   loadLeagueResources,
-  loadClubResources
 } from './data/resources';
-import BaseLeague from './data/_base/BaseLeague';
 
 Vue.use(Vuex);
 
@@ -65,26 +63,6 @@ export default new Vuex.Store({
         state.leagues = loadLeagues(api);
         state.league = defaultLeague;
       }
-    },
-    changeLeague: (state, leagueEl) => {
-      const league = leagueEl.target.value;
-      const foundLeague = state.leagues.find(l => l.key == league);
-      const loadedLeague = loadLeagueResources(state.api, foundLeague);
-
-      state.league = loadedLeague;
-      state.clubs = [];
-      state.club = {};
-      state.compareClub = {};
-
-      state.loadingClubs = true;
-      loadedLeague.loadMatches()
-        .then(matches => state.matches = matches)
-        .then(() => loadedLeague.loadClubs())
-        .then(clubs => BaseLeague.validateClubs(clubs))
-        .then(clubs => clubs.map(club => loadClubResources(state.api, state.league, club, clubs, state.matches)))
-        .then(clubs => clubs.sort((a, b) => a.nextMatch().dateObj - b.nextMatch().dateObj))
-        .then(clubs => state.clubs = clubs)
-        .then(() => state.loadingClubs = false);
     },
     selectGridLeague: (state, league) => {
       state.league = loadLeagueResources(state.api, league);
