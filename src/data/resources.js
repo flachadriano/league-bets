3dbe0937fe3816e62a533abcfad0eb1f30f1f136
Adriano Flach <adriano.spintec@oncologiador.corp>
2022-04-26 13:00:27 -0300

    remove old footballdata

--- src/data/resources.js
+++ src/data/resources.js
@@ -1,18 +1,11 @@
-import footballDataLeagues from './football-data/Leagues';
 import GridFootballData from './grid-football-data/Leagues';
 
-export const SEASON = 2021;
-
 export const apis = {
-  footballData: 'football-data',
-  footballdb: 'footballdb',
   gridFootballData: 'gridFootballData',
 };
 
 export function loadLeagues(api) {
   switch (api) {
-    case apis.footballData:
-      return footballDataLeagues;
     case apis.gridFootballData:
       return new GridFootballData();
   }
